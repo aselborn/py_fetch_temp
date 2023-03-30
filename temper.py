5106596585fd1f9da94ef7e4ecd7256b43d39a6a
Selborn Anders <k000817@ad.smhi.se>
2023-03-30 16:25:58 +0200

    download of smhi-stations

--- temper.py
+++ temper.py
@@ -1,10 +1,13 @@
 import sqlite3
-import sys
+import sys, os 
 import getopt
 import json
 import requests
+import sqlite3
 
-database_file = "C:/Users/nrobl/source/repos/tmpimport/db/Temperature.db"
+# database_file = "C:/Users/nrobl/source/repos/tmpimport/db/Temperature.db"
+database_file = os.getcwd() + "\DbTemperatur.db"
+
 dataFil = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/52230/period/latest-months/data.csv"
 
 
@@ -31,11 +34,33 @@ def download():
 def download_stations():
     headers = {'Accept':'application/json'}
     response = requests.get("https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1.json", headers=headers)
-    data = response.json()
-    print("ok")
+    rep = response.json()
+    stationer = rep["station"]
+    print("Stationer nedladdade. " )
+    return stationer
 
+def spara_stationer(stationer):
+    if not stationer is None:
+        print(database_file)
+
+
+        conn = sqlite3.connect(database_file)
+
+        
+        cur = conn.cursor()
+
+        for inp in stationer:
+            sql = "INSERT INTO Station (name, owner, ownerCategory, measuringStations, id, height, latitude, longitude, active, dtfrom, dtto, key, updated, title, summary)"
+            sql = sql + f' values {inp["name"], inp["owner"], inp["ownerCategory"], inp["measuringStations"], inp["id"], inp["height"], inp["latitude"], inp["longitude"], inp["active"], inp["from"], inp["to"], inp["key"], inp["updated"], inp["title"], inp["summary"]}'
+            print(sql)
+            cur.execute(sql)
+
+        conn.commit()
+        conn.close()
 
 if __name__ == '__main__':
     main()
-    # download()
-    download_stations()
+    stationer = download_stations()
+    spara_stationer(stationer)
+    
+
